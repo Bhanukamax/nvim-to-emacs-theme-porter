91fa8e416ade85dffb973531e8cdbe472a64d857
Bhanuka <bhanukamax@gmail.com>
2024-04-05 22:47:58 +0530

    fix

---
+++ README.md
@@ -0,0 +1,5 @@
+# Vim to Emacs Theme porter
+
+
+# How it works
+
--- export.sh
+++ export.sh
@@ -3,6 +3,7 @@
 #rm -rf theme.vim
 PIPE=/tmp/bmax-nvim.pipe
 nvim --server $PIPE --remote-send ':source ./export-highlights.vim<CR>'
+#nvim --server $PIPE --remote-send ':color gruvbox<CR>'
 nvim --server $PIPE --remote-send ':lua vim.cmd.colorscheme("gruvbuddy")<CR>'
 nvim --server $PIPE --remote-send ':call ExportHighlights("./theme.vim")<CR>'
 nvim --server $PIPE --remote-send ':qa!<CR>'
